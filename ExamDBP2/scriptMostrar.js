ee1b6180c894238aa30f94f319fa17cf2d8e212a
MisaelVM <64271736+MisaelVM@users.noreply.github.com>
2020-07-07 13:17:48 -0500

    commit

--- ExamDBP2/scriptMostrar.js
+++ ExamDBP2/scriptMostrar.js
@@ -1,11 +1,16 @@
 $(document).ready(function() {
-    $.ajax({
-        url: "http://localhost:8080/CategoryRestService/FindById/1"
-    }).then(function(data) {
-       $('.id').append(data.id);
-       $('.c-date').append(data.whenCreated);
-       $('.a-date').append(data.whenLastUpdated);
-       $('.name').append(data.name);
-       $('.description').append(data.description);
-    });
+	var content = "";
+	$.ajax({
+		url: "http://localhost:8080/CategoryRestService/FindAll"
+	}).then(function(data) {
+		$.each(data, function(i, obj){
+			var item = "<tr><th scope='row'>" + obj.id + "</th>" +
+    						"<td>" + obj.whenCreated + "</td>" +
+    						"<td>" + obj.whenLastUpdated + "</td>" +
+    						"<td>" + obj.name + "</td>" +
+    						"<td>" + obj.description + "</td>";
+    		content = content.concat(item);
+		});
+		$('.table-to-insert').append(content);
+	});
 });
